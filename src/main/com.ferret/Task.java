c2c62cdf03cd8a77d5c306d8080266878d2a1427
vykiiTor <vykiitor@gmail.com>
2023-11-15 11:04:31 +0100

    basic task and cmd constrution

--- src/main/com.ferret/Task.java
+++ src/main/com.ferret/Task.java
@@ -1,5 +1,14 @@
 package com.ferret;
 
 public class Task {
+    int id;
+    String description;
+    char status;
+
+    public void Task(int id, String description, char status){
+        this.id = id;
+        this.description = description;
+        this.status = status;
+    }
 
 }
--- src/main/com.ferret/UserCommand.java
+++ src/main/com.ferret/UserCommand.java
@@ -1,4 +1,13 @@
 package com.ferret;
 
 public class UserCommand {
+    char add='+';
+    char sub='-';
+    char done = 'x';
+    char todo = 'o';
+    char quit = 'q';
+
+    public void UserCommand(){
+        //TODO
+    }
 }
