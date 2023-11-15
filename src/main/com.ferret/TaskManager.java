a19eadeca5877cb654d9f50939430904c1b4f271
vykiiTor <vykiitor@gmail.com>
2023-11-15 11:34:49 +0100

    premier test contruction liste fail

--- src/main/com.ferret/TaskManager.java
+++ src/main/com.ferret/TaskManager.java
@@ -1,8 +1,7 @@
 package com.ferret;
 
 public class TaskManager {
-public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-
 }
--- src/test/com.ferret/TaskManagerTest.java
+++ src/test/com.ferret/TaskManagerTest.java
@@ -1,7 +1,17 @@
 package com.ferret;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskManagerTest {
-
+    String message = "Hello World";
+    @Test
+    public void testUserCommand(){
+        List<Task> tasks = new ArrayList<>();
+        assert(tasks.isEmpty());
+    }
 }
